c221171501c45cff44048e92e9c6ae06d2eb3253
Gameiro <gmeiro2002@gmail.com>
2024-05-06 12:31:48 +0100

    NavBar Size fixed

--- GAH_Clinic/src/Navbar.svelte
+++ GAH_Clinic/src/Navbar.svelte
@@ -51,6 +51,11 @@
 
 <style>
   .navbar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 1000;
     display: flex;
     justify-content: space-between;
     align-items: center;
