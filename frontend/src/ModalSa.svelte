82f4f85575dee79c13df7581afaaf803feea2809
Gameiro <gmeiro2002@gmail.com>
2024-05-14 15:19:21 +0100

    chore: Update WelcomePanel.svelte to include ModalSa component
    This commit updates the WelcomePanel.svelte file to include the ModalSa component. The previous code for navigating to the schedule appointment page has been commented out. This change enhances the functionality of the WelcomePanel by adding a modal component.

---
+++ frontend/src/ModalSa.svelte
@@ -0,0 +1,48 @@
+<script>
+  let selectedSpecialty = '';
+  let selectedDoctor = '';
+  let selectedDate = '';
+  let selectedTime = '';
+  let themeColor = 'dark'; // Isto ainda pode ser usado para outras partes do tema, se necessário.
+</script>
+
+<button class="btn btn-primary mt-4" onclick="modalSa.showModal()">
+  Agendar Consulta
+</button>
+
+<dialog id="modalSa" class="modal">
+  <div class="card w-full max-w-md bg-base-100 flex-1">
+    <div class="card-body items-center text-center p-4">
+      <h3 class="text-3xl font-bold text-secondary p-2">Schedule Your Appointment</h3>
+      <p class="font-bold text-sm text-base-content">Fill in the details below to set up your appointment.</p>
+
+      <div class="space-y-3 mt-3">
+        <div>
+          <label for="specialty" class="text-md text-center  font-bold text-accent">Specialty:</label>
+          <input id="specialty" type="text" bind:value={selectedSpecialty} class="input input-bordered w-full text-base-content" required>
+        </div>
+
+        <div>
+          <label for="doctor" class="text-md text-center  font-bold text-accent">Doctor:</label>
+          <input id="doctor" type="text" bind:value={selectedDoctor} class="input input-bordered w-full text-base-content" required>
+        </div>
+
+        <div>
+          <label for="date" class="text-md text-center font-bold text-accent">Date:</label>
+          <input id="date" type="date" bind:value={selectedDate} class="input input-bordered w-full text-base-content" required>
+        </div>
+
+        <div>
+          <label for="time" class="text-md text-center  font-bold text-accent">Time:</label>
+          <input id="time" type="time" bind:value={selectedTime} class="input input-bordered w-full text-base-content" required>
+        </div>
+      </div>
+
+      <div class="modal-action mt-4">
+        <form method="dialog">
+          <button type="submit" class="btn btn-primary">Set Appointment</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</dialog>
--- frontend/src/WelcomePanel.svelte
+++ frontend/src/WelcomePanel.svelte
@@ -1,19 +1,25 @@
 <script>
   import { navigate } from "svelte-routing";
+  import ModalSa from "./ModalSa.svelte";
 
   export let userName;
 
-  function goToAppointments() {
-    console.log("Agendar Consulta");
-    navigate("/schedule-appointment");
-  }
+  // function goToAppointments() {
+  //   console.log("Agendar Consulta");
+  //   navigate("/schedule-appointment");
+  // }
 </script>
 
 <div class="card bg-base-200 shadow-2xl shadow-base-100 flex-1">
   <div class="card-body items-center text-center flex-col">
-    <h2 class="card-title text-base-content text-4xl">Bem-vindo <span class="text-primary">{userName}</span></h2>
-    <p class="text-base-content font-bold">Hoje é um ótimo dia para cuidar da sua saúde e bem-estar.</p>
-    <button class="btn btn-primary mt-4" on:click={goToAppointments}>Agendar Consulta</button>
+    <h2 class="card-title text-base-content text-4xl">
+      Bem-vindo <span class="text-primary">{userName}</span>
+    </h2>
+    <p class="text-base-content font-bold">
+      Hoje é um ótimo dia para cuidar da sua saúde e bem-estar.
+    </p>
+    <!-- <button class="btn btn-primary mt-4">Agendar Consulta</button> -->
+    <ModalSa />
   </div>
 </div>
 
